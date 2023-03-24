172707e47f28a5ef9b7b9e525c0c6a76a382674d
elgato <amzfba.1bestbuy@gmail.com>
2023-03-24 21:30:33 +0100

    make entities

--- src/Entity/OrderDetails.php
+++ src/Entity/OrderDetails.php
@@ -8,26 +8,22 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: OrderDetailsRepository::class)]
 class OrderDetails
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
     #[ORM\Column]
     private ?int $price = null;
 
+    /**
+     * cle primaire composite
+     */
+    #[ORM\Id]
     #[ORM\ManyToOne(inversedBy: 'orderDetails')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Order $orderRef = null;
 
+    #[ORM\Id]
     #[ORM\ManyToOne(inversedBy: 'orderDetails')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Product $product = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
     public function getPrice(): ?int
     {
