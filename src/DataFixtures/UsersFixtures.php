10d88582bfad7483d8c09f250e7adbbcd9fc8c57
elgato <amzfba.1bestbuy@gmail.com>
2023-03-29 14:49:34 +0200

    fixtures

--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -2,15 +2,30 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class UsersFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $passwordEncoder,
+        private SluggerInterface            $slugger)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $admin = new User();
+        $admin->setEmail('monemail@gmail.com');
+        $admin->setLastname('prenomtest');
+        $admin->setFirstname('nomtest');
+        $admin->setAddress('1 rue hoche');
+        $admin->setCity('Paris');
+        $admin->setZipcode('75001');
+        $admin->setPassword($this->passwordEncoder->hashPassword($admin,'admin'));
 
         $manager->flush();
     }
