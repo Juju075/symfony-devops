07f6b3064b49d1318aa4d52165cd25f8a9c0f510
elgato <amzfba.1bestbuy@gmail.com>
2023-03-29 15:28:05 +0200

    fixtures

--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -7,25 +7,48 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Faker;
 
 class UsersFixtures extends Fixture
 {
     public function __construct(
-        private UserPasswordHasherInterface $passwordEncoder,
-        private SluggerInterface            $slugger)
+        private readonly UserPasswordHasherInterface $passwordEncoder,
+        private SluggerInterface $slugger)
     {
     }
 
     public function load(ObjectManager $manager): void
     {
-        $admin = new User();
-        $admin->setEmail('monemail@gmail.com');
-        $admin->setLastname('prenomtest');
-        $admin->setFirstname('nomtest');
-        $admin->setAddress('1 rue hoche');
-        $admin->setCity('Paris');
-        $admin->setZipcode('75001');
-        $admin->setPassword($this->passwordEncoder->hashPassword($admin,'admin'));
+
+
+
+        $manager->persist($user);
+
+
+        $faker = Faker\Factory::create('en_US');
+        for ($usr = 1; $usr <= 5; $usr++)
+        {
+            $user = new User();
+
+            if ($usr=1)
+            {
+                $role = ['ROLE_ADMIN','admin'];
+            }else{
+                $role  = ['ROLE_USER', 'secret'];
+            }
+
+            $faker = Faker\Factory::create('en_US');
+
+            //TODO named param
+            $user->setRoles([$role[1]]);
+            $user->setEmail($faker->email);
+            $user->setLastname($faker->lastName);
+            $user->setFirstname($faker->firstName);
+            $user->setAddress($faker->streetAddress);
+            $user->setCity($faker->city);
+            $user->setZipcode(str_replace(' ','', $faker->postcode));
+            $user->setPassword($this->passwordEncoder->hashPassword($user,$role[2]));
+        };
 
         $manager->flush();
     }
