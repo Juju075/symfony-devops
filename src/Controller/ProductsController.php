76fd2b07d5bc9e0a051b558031de616bfa256c46
elgato <amzfba.1bestbuy@gmail.com>
2023-03-30 12:08:26 +0200

    Make controllers

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/products', name: 'products_')]
 class ProductsController extends AbstractController
 {
-    #[Route('/products', name: 'app_products')]
+    //sous chemin
+
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('products/index.html.twig', [
             'controller_name' => 'ProductsController',
         ]);
     }
+
+    //DI Abstract controller
+    #[Route('/{slug}', name: 'details')]
+    public function details(Product $product): Response
+    {
+        return $this->render('products/slug.html.twig');
+    }
+
 }
