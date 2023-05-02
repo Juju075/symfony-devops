7eaef80225f0a8891b89c764a8cb637785b91270
elgato <amzfba.1bestbuy@gmail.com>
2023-05-02 17:04:53 +0200

    in progress

--- docker/symfony.sh
+++ docker/symfony.sh
@@ -1,4 +1,4 @@
-php bin/console doctrine:migrations:migrate --no-interaction
+#php bin/console doctrine:migrations:migrate --no-interaction
 # fixture pour la production
-php bin/console doctrine:fixtures:load
+#php bin/console doctrine:fixtures:load
 exec apache2-foreground
